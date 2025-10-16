7fd3c57a963c4db943ea2cc6140cabe74136634d
Priyanshu Singh <priyanshu7829singh@gmail.com>
2025-10-16 16:53:17 +0530

    Crypto Zombies contracts for practice

---
+++ Crypto_Zombies/contracts/ZombieFactory.sol
@@ -0,0 +1,41 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+contract ZombieFactory{
+
+    uint256 constant DNA_MODULUS=10**16;
+
+    struct Zombie{
+        string name;
+        uint256 dna;
+    }
+    Zombie[] public zombies;
+
+    mapping(uint256 id => address owner) public  zombieToOwner; 
+    mapping(address => uint256) public ownerZombieCount; 
+
+    modifier firstZombie(){
+        if (ownerZombieCount[msg.sender]!=0){
+            revert("You already have a zombie");
+        }
+        _;
+    }
+
+    function _randDna(string memory _str)internal pure returns(uint256){
+        uint256 dna= (uint256(keccak256(abi.encodePacked(_str))))%DNA_MODULUS;
+        return dna-dna%100;
+    }
+
+    function _createZombie(string memory _name,uint256 _dna)internal{
+        zombies.push(Zombie(_name,_dna));
+        uint256 newZombieId=zombies.length-1;
+        ownerZombieCount[msg.sender]++;
+        zombieToOwner[newZombieId]=msg.sender;
+    }
+
+    function createFirstZombie(string memory _name)public firstZombie{
+        uint256 dna= _randDna(_name);
+        _createZombie(_name,dna);
+    }
+}
