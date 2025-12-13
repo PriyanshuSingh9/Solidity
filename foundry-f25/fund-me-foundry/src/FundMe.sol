a9bf9da29ea5a8f2e7223098d6f8f3aea2dbdeac
Priyanshu Singh <priyanshu7829singh@gmail.com>
2025-12-13 19:59:18 +0530

    foundry: wrote some basic tests

--- foundry-f25/fund-me-foundry/src/FundMe.sol
+++ foundry-f25/fund-me-foundry/src/FundMe.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.24;
 
 import {PriceConverter} from "./PriceConverter.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 error FundMe__NotOwner();
 
@@ -48,6 +49,13 @@ contract FundMe {
             value: address(this).balance
         }("");
         require(callSuccess, "Call Failed");
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        return priceFeed.version();
     }
 
     receive() external payable {
--- foundry-f25/fund-me-foundry/test/FundMeTest.t.sol
+++ foundry-f25/fund-me-foundry/test/FundMeTest.t.sol
@@ -2,17 +2,19 @@
 
 pragma solidity ^0.8.24;
 
+// imprting the test and console module
+// test module brings with itself a suite of testing methods and console brings the browser functionality
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    // this function is always executed before any tests are performed
     function setUp() external {
         fundMe = new FundMe();
+        // owner of fundMe is test and not us
     }
-
-    // this function is always executed before any tests are performed
 
     function testMinimumUsdIsFive() public view {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
@@ -20,7 +22,12 @@ contract FundMeTest is Test {
 
     function testOwnerIsMsgSender() public view {
         assertEq(fundMe.i_owner(), address(this));
-        // msg.sender does not work because here teh msg sender will be us while the contract is deplyed
+        // msg.sender does not work because here the msg sender will be us while the contract is deplyed
         // by the test contract thus we use address(this) to get the address sof our test contract
+    }
+
+    function testPriceFeedVersionIsAccurate() public view {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
     }
 }
